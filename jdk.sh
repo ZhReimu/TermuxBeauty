b9a2fd5a4d8bee2d88f58c47aad75bc451bf5abb
博麗靈夢 <1725338233@qq.com>
2021-02-18 17:17:04 +0800

    修复运行 jdk.sh 后出现的 php 以及一些其他软件无法运行的问题

--- jdk.sh
+++ jdk.sh
@@ -7,7 +7,7 @@ echo "移动完成！"
 echo "正在设置环境变量"
 echo 'export JAVA_HOME="$PREFIX/share/jdk"'>>$HOME/.zshrc
 echo 'export PATH="$PATH:$JAVA_HOME/bin"'>>$HOME/.zshrc
-echo 'export LD_LIBRARY_PATH="$JAVA_HOME/lib:$JAVA_HOME/lib/jli"'>>$HOME/.zshrc
+echo 'export LD_LIBRARY_PATH="$PREFIX/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/jli"'>>$HOME/.zshrc
 echo "环境变量设置完成，请重启 termux 以完成操作！"
 echo "正在测试 Java 是否正常安装"
 java --version
